1cbb673494b60b618fac7790cabd4df2b115d56b
Orell Garten <orellgarten@gmx.net>
2022-02-02 11:54:35 +0100

    producer.py

--- producer.py
+++ producer.py
@@ -1,1 +1,42 @@
-EADME
+from confluent_kafka import Producer
+import os
+import sys
+
+
+if __name__ == '__main__':
+    topic = os.environ['KAFKA_TOPIC'].split(",")[0]
+
+    # Consumer configuration
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    conf = {
+        'bootstrap.servers': os.environ['KAFKA_BROKERS'],
+        'session.timeout.ms': 6000,
+        'default.topic.config': {'auto.offset.reset': 'smallest'},
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'SCRAM-SHA-256',
+        'sasl.username': os.environ['KAFKA_USERNAME'],
+        'sasl.password': os.environ['KAFKA_PASSWORD']
+    }
+
+    p = Producer(**conf)
+
+    def delivery_callback(err, msg):
+        if err:
+            sys.stderr.write('%% Message failed delivery: %s\n' % err)
+        else:
+            sys.stderr.write('%% Message delivered to %s [%d]\n' %
+                             (msg.topic(), msg.partition()))
+
+
+    for i in range(1000):
+        try:
+            p.produce(topic, i.to_bytes(10, byteorder="big"), callback=delivery_callback)
+        except BufferError as e:
+            sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
+                             len(p))
+        p.poll(0)
+        import time
+        time.sleep(2)
+
+    sys.stderr.write('%% Waiting for %d deliveries\n' % len(p))
+    p.flush()
